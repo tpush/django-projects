a7fbae7b9e5cc0bd9092baf3b4b81ef70fb08b46
tpush <tpushkar636@gmail.com>
2021-10-10 17:43:20 +0530

    routes in place

--- django-poll/pollster/polls/urls.py
+++ django-poll/pollster/polls/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import HomeTemplateView, QuestionListView, ChoiceDetailView
 from . import views
 
-
 urlpatterns = [
     path('', HomeTemplateView.as_view(), name='polls-home'),
     path('polls/', QuestionListView.as_view(), name='polls-detail'),
     path('polls/<int:pk>/', ChoiceDetailView.as_view(), name='polls-option'),
+    path('polls/<int:question_id>/results',
+         views.results, name='polls-result'),
+    path('polls/<int:question_id>/votes', views.votes, name='polls-vote'),
 ]
--- django-poll/pollster/polls/views.py
+++ django-poll/pollster/polls/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView, ListView, DetailView
-from .models import Question
+from .models import Question, Choice
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
 
 
 class HomeTemplateView(TemplateView):
@@ -13,3 +15,21 @@ class QuestionListView(ListView):     # <app><model>_<viewtype>.html
 
 class ChoiceDetailView(DetailView):  # <app><model>_<viewtype>.html
     model = Question
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
+def votes(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/question_detail.html', {'question': question,
+                                                              'error_message': "You didn't select a choice."})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return redirect('polls/results.html', args=(question.id,))
